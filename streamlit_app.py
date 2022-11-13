246b2e224c0b384f721a1b076174b921bc18b8ad
Fatima El garti <73041687+felgarti@users.noreply.github.com>
2022-11-13 20:24:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 
 import streamlit
+import pandas 
+
 streamlit.title("My parents' new healthy diner " ) 
 streamlit.header('Breakfast Menu')
  
@@ -10,3 +12,5 @@ streamlit.text(' 🥑🍞 Avocado toast ' )
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
